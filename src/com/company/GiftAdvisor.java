e607797e9b328a91d2d51c1085d9ba689e1ac835
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-01-28 08:35:20 -0500

    initail commit

---
+++ src/com/company/GiftAdvisor.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class GiftAdvisor {
+    private String gift;
+    private char gender;
+    private double price;
+
+    public GiftAdvisor() {
+    }
+
+    public GiftAdvisor(String gift, char gender, double price) {
+        this.gift = gift;
+        this.gender = gender;
+        this.price = price;
+    }
+
+    public String getGift() {
+        return gift;
+    }
+
+    public void setGift(String gift) {
+        this.gift = gift;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
